3311a15575a3850b3503b567bf21f13a22373486
Sinus44 <59234792+Sinus44@users.noreply.github.com>
2022-11-15 19:58:04 +0300

    Add files via upload
    Без функции, чистый код, чутка оптимизации, коментарии к коду, авто изменение размера консоли

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from PIL import Image
-from colorama import Fore,init
-from sys import argv
+# 15.11.2022 ©Sinus44
+# Параметры запуска: {путь к картинке} {ширина в символах} {яркость}
+# пример: py main.py photo.jpg 200 1
 
-args = argv # получаем параметры запуска 
+from PIL import Image
+from sys import argv
+import os
+from colorama import Fore,init
 init() # Инициализация Coloram'ы
 
 # Возвращает 1 из 8 цветом взависимости от входного стандратного 24 битного
-def getColor(color):
+def getColor(color,brightness):
     c = []
     for a in color:
-        c.append(round(a/255*1.4))
+        c.append(max(0, min(1, round(a/255*brightness))))
         
     s = " ".join(str(x)for x in c)
     if s=="0 0 1": return Fore.BLUE
@@ -21,30 +24,30 @@ def getColor(color):
     elif s=="1 1 1": return Fore.WHITE
     else: return Fore.BLACK
 
-def toAscii(path,width,height=0,alphabet=""):
-    file = Image.open(path) # Получаем файл из пути
-    img = file.load() # получение двухмерного списка с цветами пикселей 
-    
-    w = file.size[0]-1 # ширина оригинального изо -1
-    h = file.size[1]-1 # высота оригинального изо -1
+path = argv[1]
+width = int(argv[2])
+alphabet = " .-:!/r(l1Z4H9W8$@"
+brightness = float(argv[3])
 
-    symbolRate = 5/2 # соотношение символов
-    originalRate = w/h # соотношение оригинальное
-    rate = round(originalRate*symbolRate) # соотношение итоговое
-    
-    if height == 0: # если высота не указана
-        height = round(width/rate) # высчитываем высоту изо в консоли
+file = Image.open(path) # Получаем файл из пути
+img = file.load() # получение двухмерного списка с цветами пикселей 
 
-    for y in range(height):
-        line = "" # инициализируем пустую строку
-        for x in range(width):
-            #OnOriginalImage
-            xOOI = round((x/width) * w) # координата X на оригинальном изо
-            yOOI = round((y/height) * h) # координата Y на оригинальном изо
+w = file.size[0]-1 # ширина оригинального изо -1
+h = file.size[1]-1 # высота оригинального изо -1
 
-            color = img[xOOI,yOOI] # Получаем цвет пикселя в оригинальном изо
-            shade = (color[0]+color[1]+color[2])/(255*3) # Преобразуем в значение от 0 до 1
-            line = line + getColor(color) + alphabet[round(shade*len(alphabet)-1)] # прибавляем к строке соответсвующий символ
-        print(line) # Отображаем строку
-        
-toAscii(args[1],int(args[2]), alphabet = " .-:!/r(l1Z4H9W8$@") # вызываем функцию отрисовки
+symbolRate = 5/2 # соотношение символов
+originalRate = w/h # соотношение оригинальное
+rate = round(originalRate*symbolRate) # соотношение итоговое
+height = round(width/rate) # высчитываем высоту изо в консоли
+
+os.system(f"mode con cols={width} lines={height}")
+
+for y in range(height):
+    line = "" # инициализируем пустую строку
+    for x in range(width):
+    	xOOI = round((x/width) * w) # координата X на оригинальном изо  #OnOriginalImage
+    	yOOI = round((y/height) * h) # координата Y на оригинальном изо
+		color = img[xOOI,yOOI] # Получаем цвет пикселя в оригинальном изо
+		shade = (color[0]+color[1]+color[2])/(255*3) # Преобразуем в значение от 0 до 1
+		line += getColor(color,brightness) + alphabet[round(shade*(len(alphabet)-1))] # прибавляем к строке соответсвующий символ
+    print(line) # Отображаем строку
